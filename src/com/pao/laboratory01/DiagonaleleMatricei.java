2b397503750dfb06add398c500797255e5ef0e61
Antonia <antonia.trifan@pdq.com>
2026-03-30 20:56:37 +0300

    practiced lab0 no testing yet

--- src/com/pao/laboratory01/DiagonaleleMatricei.java
+++ src/com/pao/laboratory01/DiagonaleleMatricei.java
@@ -1,4 +1,6 @@
-package com.pao.laboratory00;
+package com.pao.laboratory01;
+
+import java.util.Scanner;
 
 /**
  * Exercitiul 2
@@ -13,6 +15,29 @@ package com.pao.laboratory00;
 
 public class DiagonaleleMatricei {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n;
+        double sumaPrinc = 0 , produsSec = 1.0;
+        double[][] array;
 
+        n = scanner.nextInt();
+
+        array = new double[n][n];        
+        for (int i = 0 ; i < n; i++)
+            for (int j = 0 ; j < n; j++)
+            {
+                array[i][j] = scanner.nextDouble();
+                if (i == j)
+                    sumaPrinc += array[i][i];
+                if (i == ((n-j) - 1))
+                    produsSec *= array[i][j];
+            }
+
+        for (int i = 0 ; i < n; i++)
+            for (int j = 0 ; j < n; j++)
+                System.out.println(array[i][j]);
+        
+        System.out.println(sumaPrinc);
+        System.out.println(produsSec);
     }
 }
--- src/com/pao/laboratory01/MediaAritmetica.java
+++ src/com/pao/laboratory01/MediaAritmetica.java
@@ -1,5 +1,6 @@
-package com.pao.laboratory00;
+package com.pao.laboratory01;
 
+import java.util.Scanner;
 /**
  * Exercitiul 1
  *
@@ -12,6 +13,26 @@ package com.pao.laboratory00;
 
 public class MediaAritmetica {
     public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);   
+        int n, suma = 0;
+        int[] array;
 
+        n = scanner.nextInt();
+        array = new int[n];
+        for (int i = 0 ; i < n; i++)
+        {
+            array[i] = scanner.nextInt();
+            suma += array[i];
+        }
+
+        // afisare cu index
+        for (int i = 0; i < n; i++)
+            System.out.println(array[i]);
+
+        // afisare enhanced for
+        for (int num : array)
+            System.out.println(num);
+    
+        System.out.println((double)(suma/n));
     }
 }
