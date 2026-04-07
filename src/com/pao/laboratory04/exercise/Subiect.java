7d386a8ac82faf7a91c7e515c546c3df351bf3cf
Antonia <antonia.trifan@pdq.com>
2026-04-07 18:09:54 +0300

    started ex4 in lab4 practice

---
+++ src/com/pao/laboratory04/exercise/Subiect.java
@@ -0,0 +1,13 @@
+package com.pao.laboratory04.exercise;
+
+public class Subiect
+{
+    private enum Materii{
+        PAOJ, BD, SO, RC2ParameterSpec
+    }
+    String fullName;
+    int credits;
+
+    private Subiect()
+    {}
+}
